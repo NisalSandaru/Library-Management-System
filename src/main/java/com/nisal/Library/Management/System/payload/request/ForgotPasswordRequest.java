9c13ad7545d2d01a6d79bd73792a53bad72a8037
User <nisalsandaru1@gmail.com>
2026-04-06 03:27:17 +0530

    forgot pass

--- src/main/java/com/nisal/Library/Management/System/payload/request/ForgotPasswordRequest.java
+++ src/main/java/com/nisal/Library/Management/System/payload/request/ForgotPasswordRequest.java
@@ -1,4 +1,12 @@
 package com.nisal.Library.Management.System.payload.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ForgotPasswordRequest {
+    private String email;
 }
